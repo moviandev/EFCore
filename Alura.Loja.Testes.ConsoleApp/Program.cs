30eae5f443d4b510cab8b8d4b2e337083c248b83
Matheus Viana <matheus_o_viana@hotmail.com>
2021-01-17 23:08:48 -0300

    added first data with EF Core in SQL Server

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -10,19 +10,21 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            GravarUsandoAdoNet();
+            //GravarUsandoAdoNet();
+            GravarUsandoEntity();
         }
 
-        private static void GravarUsandoAdoNet()
+        private static void GravarUsandoEntity()
         {
             Produto p = new Produto();
             p.Nome = "Harry Potter e a Ordem da Fênix";
             p.Categoria = "Livros";
             p.Preco = 19.89;
 
-            using (var repo = new ProdutoDAO())
+            using (var context = new LojaContext())
             {
-                repo.Adicionar(p);
+                context.Produtos.Add(p);
+                context.SaveChanges();
             }
         }
     }
